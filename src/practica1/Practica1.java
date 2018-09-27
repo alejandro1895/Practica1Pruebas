c4186fdb9a5e9c482a141aa28db1fbc453f47eb1
raulms10 <rantonio.martinez@udea.edu.co>
2018-09-27 14:28:08 -0500

    Funciones de Cuadriláteros

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -1,104 +1,72 @@
 package practica1;
 
-import java.util.Scanner;
+import practica1.constantes.Mensajes;
+import practica1.main.Funciones;
 
 /**
- *
- * @author hp
+ * Clase principal, desde aquí se inicia la aplicación
+ * @author Raúl, Alejandro
  */
 public class Practica1 {
-
     
     public static void main(String[] args) throws InterruptedException {
         // Se instancia la clase Funciones
-        Funciones fc = new Funciones();   
-        //Se lee la entrada del teclado
-        Scanner sc = new Scanner(System.in);
+        Funciones fc = new Funciones();
         
-        String opcion;
-        int opcionInt=0;
+        double lado1, lado2, lado3, lado4;
+        int opcion;
         
-        //El ciclo solo se rompe cuando el usuario o el mismo programa eligen la opcion salir (3)
-        while(opcionInt!=3){
-            
-            
-            fc.Presentacion();
+        fc.mostrarMensaje(Mensajes.ENCABEZADO + Mensajes.SALTO_DE_LINEA);
         
-            //El usuario escoge una opcion del menu
-            System.out.print("Digite la opción: ");
-            opcion = sc.nextLine();             
-            //Se valida la entrada
-            if(!fc.esValida(opcion)){   
-                //En caso de entrada erronea
-                System.out.print("Digite nuevamente la opción: ");
-                opcion = sc.nextLine();
-            }else{                
-                //Ingresa como opcion valida
-                
-                //Se requiere saber que opcion es (1,2 o 3)
-                opcionInt=Integer.parseInt(opcion);
-                
-                if(opcionInt==1){ //3 lados
-                    
-                    //Los string nos ayudan a validar la entrada, los float guardan el valor
-                    String ladoa, ladob, ladoc;
-                    float a,b,c;
-                    
-                    //Lado 1
-                    System.out.print("Digite el lado #1: ");
-                    ladoa=sc.nextLine();
-                    while(!fc.esLadoValido(ladoa)){
-                        System.out.print("Digite nuevamente el lado #1");
-                        ladoa = sc.nextLine();
-                    }
-                    a = Float.parseFloat(ladoa);
-                    
-                    //Lado 2
-                    System.out.print("Digite el lado #2: ");
-                    ladob=sc.nextLine();
-                    while(!fc.esLadoValido(ladob)){
-                        System.out.print("Digite nuevamente el lado #2");
-                        ladob = sc.nextLine();
-                    }
-                    b = Float.parseFloat(ladob);
+        //El ciclo solo se rompe cuando el usuario elige la opción salir (3)
+        do{
+            fc.mostrarMensaje(Mensajes.OPCIONES_MENU + Mensajes.SALTO_DE_LINEA + Mensajes.SALTO_DE_LINEA);
+            opcion = fc.leerOpcion();
 
-                    //Lado 3
-                    System.out.print("Digite el lado #3: ");
-                    ladoc=sc.nextLine();
-                    while(!fc.esLadoValido(ladoc)){
-                        System.out.print("Digite nuevamente el lado #3");
-                        ladoc = sc.nextLine();
-                    }
-                    c = Float.parseFloat(ladoc);    
+            switch (opcion) {
+                case 1: //3 lados
+                    fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + Mensajes.PARA_TRIANGULOS + Mensajes.SALTO_DE_LINEA);
+
+                    lado1 = fc.leerLado(Mensajes.LADO_1);
+                    lado2 = fc.leerLado(Mensajes.LADO_2);
+                    lado3 = fc.leerLado(Mensajes.LADO_3);
+
+                    //Teniendo los 3 lados del triángulo, se define que tipo es
+                    if(fc.esEquilatero(lado1, lado2, lado3)){
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeTriangulos(lado1, lado2, lado3) + Mensajes.EQUILATERO + Mensajes.SALTO_DE_LINEA);
+                    }else if(fc.esIsosceles(lado1, lado2, lado3)){
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeTriangulos(lado1, lado2, lado3) + Mensajes.ISOSCELES + Mensajes.SALTO_DE_LINEA);
+                    }else if(fc.esEscaleno(lado1, lado2, lado3)){
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeTriangulos(lado1, lado2, lado3) + Mensajes.ESCALENO + Mensajes.SALTO_DE_LINEA);
+                    }else{
+                        fc.mostrarMensaje(Mensajes.NO_TRIANGULO + Mensajes.SALTO_DE_LINEA);
+                    }   Thread.sleep(2000); //Pausa de 2 segundos
+                    break;
                     
-                    //Teniendo los 3 lados del triangulo, se define que tipo es
-                    if(fc.esEquilatero(a,b,c)){
-                        System.out.println("Es un triangulo equilatero \n");
-                        Thread.sleep(3000); //Pausa de 3 segundos
-                    }else if(fc.esIsosceles(a,b,c)){
-                        System.out.println("Es un triangulo isosceles \n");
-                        Thread.sleep(3000); //Pausa de 3 segundos
-                    }     
-                    if(fc.esEscaleno(a,b,c)){
-                        System.out.println("Es un triangulo escaleno \n");    
-                        Thread.sleep(3000); //Pausa de 3 segundos
-                    }                    
-                }
-                
-                if(opcionInt==2){  //4 lados
+                case 2: //4 lados
+                    fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + Mensajes.PARA_CUADRILATEROS + Mensajes.SALTO_DE_LINEA);
                     
-                }
+                    lado1 = fc.leerLado(Mensajes.LADO_1);
+                    lado2 = fc.leerLado(Mensajes.LADO_2);
+                    lado3 = fc.leerLado(Mensajes.LADO_3);
+                    lado4 = fc.leerLado(Mensajes.LADO_4);
+                    
+                    if(fc.esCuadrado(lado1, lado2, lado3, lado4)){
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeCuadrilatero(lado1, lado2, lado3, lado4) + Mensajes.CUADRADO + Mensajes.SALTO_DE_LINEA);
+                    }else if(fc.esRectangulo(lado1, lado2, lado3, lado4)){
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeCuadrilatero(lado1, lado2, lado3, lado4) + Mensajes.RECTANGULO + Mensajes.SALTO_DE_LINEA);
+                    }else if(fc.esTrapecio(lado1, lado2, lado3, lado4)){
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeCuadrilatero(lado1, lado2, lado3, lado4) + Mensajes.TRAPECIO + Mensajes.SALTO_DE_LINEA);
+                    }else{
+                        fc.mostrarMensaje(Mensajes.SALTO_DE_LINEA + fc.mensajeCuadrilatero(lado1, lado2, lado3, lado4) + Mensajes.NO_CUADRILATERO + Mensajes.SALTO_DE_LINEA);
+                    }   Thread.sleep(2000); //Pausa de 2 segundos
+                    break;
                 
-                if(opcionInt==3){  //Opcion Salir
-                    System.out.println("Saliendo del sistema...");
+                default: //Opcion Salir
+                    fc.mostrarMensaje(Mensajes.SALIENDO + Mensajes.SALTO_DE_LINEA);
                     Thread.sleep(5000); //Pausa de 5 segundos
-                }
-                                
-            }
-
-        }
-     
-        
+                    break;
+            }             
+        }while(opcion!=3);
     }
-    
 }
